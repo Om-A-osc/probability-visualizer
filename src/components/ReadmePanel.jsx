cccd6351169bdc8af9a562ae5fae86b3a6fa2ef0
Om-A-osc <omanand8132@gmail.com>
2026-03-18 19:22:37 +0530

    chore: remove shims and simplify vite config

--- src/components/ReadmePanel.jsx
+++ src/components/ReadmePanel.jsx
@@ -4,7 +4,7 @@ import rehypeRaw from "rehype-raw";
 
 const readmeModules = import.meta.glob(
   "../../node_modules/@stdlib/stats/base/dists/**/README.md",
-  { as: "raw" }
+  { query: "?raw", import: "default" }
 );
 
 const readmeMap = {};
--- src/shims/stdlibFmtProdMsg.js
+++
@@ -1,3 +0,0 @@
-export default function fmtProdMsg(code) {
-  return `stdlib error ${code}`;
-}
